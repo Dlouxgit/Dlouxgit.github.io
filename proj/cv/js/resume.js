6ca386fb92683116d5162a3c920c3edda91df886
Dloux <7564242+Dloux@user.noreply.gitee.com>
2020-09-13 16:21:16 +0800

    Site updated: 2020-09-13 16:20:09

---
+++ proj/cv/js/resume.js
@@ -0,0 +1,24 @@
+var button = document.getElementsByClassName("fa-compass")[0];
+var contact = document.getElementsByClassName("contact")[0];
+var returnButton = document.getElementsByClassName("return")[0];
+var returnButton2 = document.getElementsByClassName("button")[0];
+returnButton.addEventListener("click", resumePut);
+returnButton2.addEventListener("click", resumePut);
+button.addEventListener("click", resumePut);
+var flag = true;
+var count = 0;
+function resumePut() {
+    if(flag){
+        flag = false;
+        if(count%2 == 0){
+            contact.style.cssText = "transition: all .5s ease-out;-webkit-transition: all .5s ease-out;";
+            contact.className="contact contact-active ";
+            returnButton.className="return return-active";
+        }else {
+            contact.className="contact";
+            returnButton.className="return";
+        }
+        count++;
+    }
+    setTimeout("flag=true;",400);
+}
